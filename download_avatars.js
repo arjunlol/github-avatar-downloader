05ea20877cbb44c38d9a6ecb091e90129bc31b4e
Arjun Lall <lallarjun@gmail.com>
2017-05-08 21:59:05 +0000

    added function to download avatar at url to filepath given, tested function with hardcoded values

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
-//import request module
+//import request module & fs
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -25,13 +26,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   }
 
-  //logs body of JSON
+  //passes error and parsed JSON body to callback function defined below
   request.get(options, function (err, response) {
     cb(err, JSON.parse(response.body));
   });
 
 
 }
+
+//function will make request to given URL, saving resulting image file to path
+function downloadImageByURL(url, filepath) {
+  request.get(url)
+    .pipe(fs.createWriteStream("./avatars/" + filepath));
+}
+
+//test hard coded image download function
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466","kvirani.jpg");
 
 //invoking getRepoContributors function using hard code values
 getRepoContributors("jquery", "jquery", function(err, result) {
