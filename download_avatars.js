23632ad735c4a59aa53c4842337e4d69d002b855
Arjun Lall <lallarjun@gmail.com>
2017-05-08 22:48:59 +0000

    added config file to store username and token credentials using dotenv npm package

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,14 @@
 //import request module & fs
 var request = require('request');
 var fs = require('fs');
+require('dotenv').config();
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 //github username here
-var GITHUB_USER = "arjunlol";
+var GITHUB_USER = process.env.USERNAME;
 //github token here
-var GITHUB_TOKEN = "02adcf4559363526c768a4aca3f705afe7f689d2";
+var GITHUB_TOKEN = process.env.TOKEN;
 //user Agent variable
 var USER_AGENT = 'GitHub Avatar Downloader - Student Project';
 //accepting command line arguments for the owner of the repo and the reponame respectively
@@ -34,13 +35,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 //function will make request to given URL, saving resulting image file to path
-function downloadImageByURL(url, filepath) {
+function downloadImageByURL (url, filepath) {
   request.get(url)
     .pipe(fs.createWriteStream("./avatars/" + filepath));
 }
 
 //invoking getRepoContributors function using hard code values
-getRepoContributors(OWNER_AND_REPONAME[0], OWNER_AND_REPONAME[1], function(err, result) {
+getRepoContributors (OWNER_AND_REPONAME[0], OWNER_AND_REPONAME[1], function(err, result) {
+  //does not execute callback if arguments for owner and repo not passed in command line
   if(!OWNER_AND_REPONAME[0] && !OWNER_AND_REPONAME[1]){
     console.log("Please input arguments for both the owner and the name of the repo, respectively");
     console.log('Thanks.')
