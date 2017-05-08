68ddadb4c6e41a9a042e2026708b35ddc481251e
Arjun Lall <lallarjun@gmail.com>
2017-05-08 20:25:40 +0000

    added skeleton of function to return repo contributors + invoked function using hard coded values

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,16 @@
+//import request module
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+//function uses request library to fetch list of contributors via HTTPS for respective repo
+//cb is a callback function to handle asynchronous result returns
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+//invoking getRepoContributors function using hard code values
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
