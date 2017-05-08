3578c4c89dffb0bdae53db097aa6e2740d226f67
Arjun Lall <lallarjun@gmail.com>
2017-05-08 20:41:35 +0000

    tested url api to confirm returning JSON format

--- download_avatars.js
+++ download_avatars.js
@@ -3,10 +3,17 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//github username here
+var GITHUB_USER = "";
+//github token here
+var GITHUB_TOKEN = "";
+
 //function uses request library to fetch list of contributors via HTTPS for respective repo
 //cb is a callback function to handle asynchronous result returns
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN +
+    '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 //invoking getRepoContributors function using hard code values
